748732f600229a6a65de219be430f00343e8cab3
Tyler Childs <email@tychi.me>
2024-04-20 16:37:51 -0400

    create a full peer with a node server and deno client

---
+++ deno.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import braidmail from './index.js'
+
+var app = express()
+
+app.use(free_the_cors)
+
+// Host some simple HTML
+sendfile = (f) => (req, res) => res.sendFile(f, {root:'.'})
+app.get('/', sendfile('demo.html'))
+
+app.use('/public', express.static('public'))
+app.use(braidmail)
+
+// Spin up the server
+require('http')
+    .createServer(app)
+    .listen(7465, () => console.log('listening on 7465'))
+
+// Free the CORS!
+function free_the_cors (req, res, next) {
+    console.log('free the cors!', req.method, req.url)
+    res.setHeader('Range-Request-Allow-Methods', 'PATCH, PUT')
+    res.setHeader('Range-Request-Allow-Units', 'json')
+    res.setHeader("Patches", "OK")
+    var free_the_cors = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, HEAD, GET, PUT, UNSUBSCRIBE",
+        "Access-Control-Allow-Headers": "subscribe, client, version, parents, merge-type, content-type, patches, cache-control, peer"
+    }
+    Object.entries(free_the_cors).forEach(x => res.setHeader(x[0], x[1]))
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200)
+        res.end()
+    } else
+        next()
+}
