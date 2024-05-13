e58c47b2a74f9897eb5474ab7401c70cfb3a43a6
Michael Toomim <toomim@gmail.com>
2024-05-13 15:23:18 +0300

    Fetch posts in parallel vs. sequence. Much faster.

--- net-client.js
+++ net-client.js
@@ -1,12 +1,38 @@
 import * as braid from 'https://esm.sh/braid-http'
   
 var feed = []
+var posts = {}
 
 async function subscribe_to_feed (url, cb) {
 
     var retry = () => setTimeout(() => subscribe_to_feed(url, cb), 3000)
 
-    var update_feed = (update) => {
+    // Do the initial fetch
+    try {
+        var res = await braid.fetch(url, {subscribe: true})
+    }
+    catch (e) { retry() }
+
+    console.log('Got res! subscribe is', res.headers.get('subscribe'))
+
+    // Server might support subscriptions
+    if (res.headers.has('subscribe'))
+        res.subscribe(patch_feed, retry)
+
+    // Else just do polling
+    else {
+        // Incorporate this update we got
+        patch_feed({
+            version: res.version,
+            body: await res.text()
+        })
+
+        // And poll again
+        console.log('Polling!  Waiting 30 seconds...')
+        setTimeout(retry, 30 * 1000)
+    }
+
+    function patch_feed (update) {
         console.log('We got a new update!', update)
 
         if (update.body)
@@ -23,50 +49,12 @@ async function subscribe_to_feed (url, cb) {
         console.log('Now feed is', feed)
 
         // Fetch the post and announce!
-        fetch_posts(feed).then(posts => {
-            console.log('announcing with', {posts, cb})
-            cb(posts)
-        })
-    }
-
-
-    try {
-        var res = await braid.fetch('/feed', {subscribe: true})
-    } catch (e) {
-        console.log('try catch error! retrying...')
-        retry()
-    }
-
-    console.log('Got res! subscribe is', res.headers.get('subscribe'))
-
-    if (res.headers.has('subscribe'))
-        res.subscribe(update_feed, (e) => {
-            console.log('subscribe error!', e, 'retrying...')
-            retry()
-        })
-    else {
-        // Todo: add braid-http client library support to return array
-        // of updates, or iterate through sequence of updates, in the
-        // various forms that it can be encoded.
-        //
-        // Form 1:
-        //  - Normal GET response with headers and body as update
-        //
-        // Form 2:
-        //  - Get response with set of one or updates in body
-
-        update_feed({
-            version: res.version,
-            body: await res.text()
-        })
-
-        // Polling!
-        console.log('Polling!  Waiting 30 seconds...')
-        retry()
+        fetch_posts(feed, cb)
     }
 }
 
 async function fetch_post (url) {
+    console.log('fetching post', url)
     // Todo: subscribe to changes in posts
     var res = await braid.fetch(url)
     if (res.status === 200)
@@ -75,29 +63,51 @@ async function fetch_post (url) {
         return undefined
 }
   
-async function fetch_posts (feed) {
-    var result = []
+function fetch_posts (feed, cb) {
+    console.log('fetching posts for', feed)
+
+    // Initialize posts hash
     for (var i=0; i<feed.length; i++) {
-        var post = await fetch_post(feed[i].link)
-        try {
-            post = JSON.parse(post)
-        } catch (e) {
-            console.error('Error parsing post', {link: feed[i].link, post})
-            continue
+        var link = feed[i].link
+        if (!(link in posts)) {
+            console.log('initializing post', link)
+            posts[link] = undefined
         }
-        if (post)
-            result.push({url: feed[i].link, ...post})
     }
 
-    return result
+    // Fetch all missing posts
+    for (let link in posts)
+        if (!posts[link])
+            fetch_post(link).then( post => {
+                try {
+                    post = JSON.parse(post)
+                } catch (e) {
+                    console.error('Error parsing post', {link, post})
+                    return
+                }
+                posts[link] = post
+                var new_feed = compile_posts()
+                console.log('Announcing posts:', new_feed)
+                cb(new_feed)
+            })
+
+    function compile_posts () {
+        var result = []
+        for (var i=0; i<feed.length; i++) {
+            var post = posts[feed[i].link]
+            if (post)
+                result.push({url: feed[i].link, ...post})
+        }
+        return result
+    }
 }
 
-function make_new_post ({subject, body}) {
+function make_new_post (host, {subject, body}) {
     // Generate a random ID
     var id = Math.random().toString(36).substr(6)
 
     // Post it to the server
-    braid.fetch('/post/' + id, {
+    braid.fetch(host + '/post/' + id, {
         method: 'PUT',
         body: JSON.stringify({
             to:       ["rjaycarpenter@gmail.com","email@tychi.me"],
