d663fae16d7d9facdf60df83afa80be146df051a
Tyler Childs <email@tychi.me>
2024-05-16 03:33:34 -0400

    bring in some simple, yet interesting tags

--- tag.js
+++ tag.js
@@ -164,3 +164,37 @@ function sufficientlyUniqueId() {
     return v.toString(16);
   });
 }
+
+tags({ registry: '/tags' })
+new MutationObserver(() => {
+  tags({ registry: '/tags' })
+}).observe(document.body, { childList: true, subtree: true });
+function tags({ registry }) {
+  const tags = new Set(
+    [...document.querySelectorAll(':not(:defined)')]
+    .map(({ tagName }) => tagName.toLowerCase())
+  )
+
+  tags.forEach(async (tag) => {
+    const url = `${registry || '.'}/${tag}.js`
+    const exists = (await fetch(url, { method: 'HEAD' })).ok
+    if(!exists) return
+    let definable = true
+    await import(url).catch((e) => {
+      definable = false
+      console.error(e)
+    })
+    try {
+      definable = definable && document.querySelector(tag) && document.querySelector(tag).matches(':not(:defined)')
+      if(definable) {
+        customElements.define(tag, class WebComponent extends HTMLElement {
+          constructor() {
+            super();
+          }
+        });
+      }
+    } catch(e) {
+      console.log('Error defining module:', tag, e)
+    }
+  })
+}
